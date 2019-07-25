64a8ef179b0e7ab41c84b93ac6381b4e60b15c5c
Alex Hays <alexhays@gmail.com>
2019-07-25 19:25:52 -0400

    add styled-components

--- components/header.js
+++ components/header.js
@@ -1,7 +1,13 @@
+import styled from 'styled-components';
+const Title = styled.h1`
+  font-size: 50px;
+  color: ${({ theme }) => theme.colors.primary};
+`;
+
 function Header() {
   return (
     <header>
-      <h1>Next.js Example on Now 2.0</h1>
+      <Title>Next.js Example on Now 2.0</Title>
     </header>
   );
 }
